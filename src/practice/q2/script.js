e1600bbaab2677ae8c2be7439316cf44ffb7c25e
kosei <koush0811@gmail.com>
2026-04-24 18:48:21 +0900

    q2

--- src/practice/q2/script.js
+++ src/practice/q2/script.js
@@ -1,13 +1,44 @@
 const display = document.getElementById('display')
 const start = document.getElementById('start')
-const stop = document.getElementById('stop')
+const stopbtn = document.getElementById('stop')
 const reset = document.getElementById('reset')
 
 let isStart = false
-let startTime
+let startTime = 0
+let time = 0
+let IntervalID
 
 start.addEventListener('click',()=>{
-    startTime = Date.now() 
+    if(isStart) return
+    startTime = Date.now() - time
 
+    IntervalID = setInterval(()=>{
+        time = Date.now() - startTime
 
+        const ms = time % 1000
+        const totalseconds = Math.floor( time /1000)
+        const second = totalseconds % 60
+        const totalminute = Math.floor( totalseconds /60 )
+        const minute = totalminute % 60
+        const hour = Math.floor(totalminute / 60)
+
+        const StrMs = ms.toString().padStart(3,"0")
+        const StrSecond = second.toString().padStart(2,"0")
+        const StrMinute = minute.toString().padStart(2,"0")
+        const StrHour = hour.toString().padStart(2,"0")
+
+        display.textContent = `${StrHour}:${StrMinute}:${StrSecond}:${StrMs}`
+    },10)
+})
+ 
+stopbtn.addEventListener('click',()=>{
+    clearInterval(IntervalID)
+    isStart = false
+})
+
+reset.addEventListener('click',()=>{
+    clearInterval(IntervalID)
+    isStart = false
+    time = 0
+    display.textContent = "00:00:00:000"
 })
